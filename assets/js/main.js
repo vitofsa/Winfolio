1233d42e2ca796aa416163ee2625cbafcd6c2271
vitofsa <vitorfirmesa@gmail.com>
2026-06-07 15:05:38 -0300

    ✨ Alternar modos escuro/claro

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// Modo escuro / claro
+
+const modoLuz = localStorage.getItem('modo-luz');
+if (modoLuz !== null) {
+    if (modoLuz == 'light') {
+        setarModoLuz('light');
+    }
+}
+
+document.getElementById('switchModo').addEventListener('click', function() {
+    const $body = document.getElementsByTagName('body')[0];
+    const modoAtual = $body.getAttribute('data-modo');
+
+    setarModoLuz((modoAtual == 'dark') ? 'light' : 'dark');
+})
+
+function setarModoLuz(modo) {
+    const $body = document.getElementsByTagName('body')[0];
+    const $iconeBotao = document.getElementById('switchModo').getElementsByTagName('img')[0];
+
+    switch (modo) {
+        case 'dark':
+            $body.setAttribute('data-modo', 'dark');
+            localStorage.setItem('modo-luz', 'dark');
+            $iconeBotao.setAttribute('src', "https://img.icons8.com/3d-fluency/94/partly-cloudy-night.png")
+            break;
+            
+        case 'light':
+            $body.setAttribute('data-modo', 'light');
+            localStorage.setItem('modo-luz', 'light');
+            $iconeBotao.setAttribute('src', "https://img.icons8.com/3d-fluency/94/partly-cloudy-day.png")
+            break;
+            
+        default:
+            $body.setAttribute('data-modo', 'dark');
+            localStorage.setItem('modo-luz', 'dark');
+            $iconeBotao.setAttribute('src', "https://img.icons8.com/3d-fluency/94/partly-cloudy-night.png")
+            break;
+    }
+}
